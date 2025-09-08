641db5e957475468145df4e67786a3632cd78ea9
TheMateeus <105858907+TheMateeus@users.noreply.github.com>
2025-09-08 17:14:03 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,3 @@ import "./index.css";
 import "./App.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
-const originalConsoleError = console.error;
-console.error = function (...args) {
-  if (typeof args[0] === "string" && args[0].includes("404 Error")) {
-    // ignora os logs de rota inexistente
-    return;
-  }
-  originalConsoleError.apply(console, args);
-};
